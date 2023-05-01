ab08f22508150cd1a1928c69b1e97409fd999bbe
TomIshiyama <honngannji@gmail.com>
2023-05-01 22:58:09 +0900

    chore :rocket: : add a storybook package

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "storybook-solidjs-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  core: {
+    builder: "@storybook/builder-vite"
+  },
+  framework: {
+    name: "storybook-solidjs-vite",
+    options: {}
+  },
+  docs: {
+    autodocs: "tag"
+  }
+};
+export default config;
