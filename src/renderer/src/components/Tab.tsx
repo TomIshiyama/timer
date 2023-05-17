0d98f7851747a55df7e7472dcce22a59cef92305
TomIshiyama <honngannji@gmail.com>
2023-05-18 03:27:19 +0900

    :sparkles: feat: create Tabs components
    - basic implementations
    - TODO: add a style to the Tabs componetns

--- src/renderer/src/components/Tab.tsx
+++ src/renderer/src/components/Tab.tsx
@@ -1,6 +1,9 @@
-import { Component } from "solid-js";
+import { Component, For, createSignal } from "solid-js";
+import { styled } from "solid-styled-components";
+import { Tabs } from "@kobalte/core";
+import { JSX } from "solid-js/types/jsx";
 
-const menus = {
+export const tabMenus = {
   pomodoro: "pomodoro",
   timer: "timer",
   stopwatch: "stopwatch",
@@ -8,15 +11,57 @@ const menus = {
   worldClock: "worldClock"
 } as const;
 
-export const Tab: Component = () => {
-  // state どのタブなのか
+type Props = {
+  hoge: string;
+  defaultValue?: string;
+  tabList: {
+    value: string;
+    label: JSX.Element;
+    content: JSX.Element;
+  }[];
+  onChange?: (value: string) => void;
+  orientation?: "horizontal" | "vertical";
+};
+
+export const Tab: Component<Props> = (props) => {
+  const [selectedTab, setSelectedTab] = createSignal<string>(
+    // eslint-disable-next-line solid/reactivity
+    props.defaultValue ?? props.tabList[0].value
+  );
 
   return (
     <div>
-      {/* <Tabs>
-        <TabsContent>hoge</TabsContent>
-      </Tabs> */}
-      hoge
+      <StyledTabs
+        // controlled value
+        value={selectedTab()}
+        onChange={(value): void => {
+          console.log(value);
+          setSelectedTab(value);
+          props.onChange?.(value);
+        }}
+        orientation={props.orientation ?? "horizontal"}
+        aria-label="Main navigation"
+      >
+        <StyledTabList>
+          <For each={props.tabList} fallback={<div>Loading...</div>}>
+            {(item): JSX.Element => <Tabs.Trigger value={item.value}>{item.label}</Tabs.Trigger>}
+          </For>
+          <Tabs.Indicator />
+        </StyledTabList>
+        <For each={props.tabList} fallback={<div>Loading...</div>}>
+          {(item): JSX.Element => <Tabs.Content value={item.value}>{item.content}</Tabs.Content>}
+        </For>
+      </StyledTabs>
     </div>
   );
 };
+
+const StyledTabs = styled(Tabs.Root)`
+  background-color: red;
+`;
+
+const StyledTabList = styled(Tabs.List)`
+  background-color: blue;
+`;
+
+// コンポーネントの宣言が前後してもエラーがでないのか・・・？
