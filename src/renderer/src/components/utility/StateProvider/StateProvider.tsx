6f4f197e21cd19847f29414d9666c5f496c6720b
TomIshiyama <honngannji@gmail.com>
2023-08-17 21:51:22 +0900

    :sparkles feat: add a several mock constants

--- src/renderer/src/components/utility/StateProvider/StateProvider.tsx
+++ src/renderer/src/components/utility/StateProvider/StateProvider.tsx
@@ -2,23 +2,35 @@ import { createContext, onCleanup, onMount } from "solid-js";
 import { SetStoreFunction, Store, createStore } from "solid-js/store";
 import { JSX } from "solid-js";
 import { State } from "./types";
+import {
+  TIMER_RUNNING_STATUS,
+  TIMER_STATE_TRANSITION
+} from "@renderer/components/page/pomodoro/type";
 
 type ContextType<T = object> = {
   state: Store<T>;
   setState: SetStoreFunction<T>;
 };
 
-const initialState: State = {
-  pomodoro: {
-    remainingTime: 0,
-    isRunning: false,
-    isFinished: false,
-    isPaused: false,
-    round: {
-      current: 0,
-      limit: 0
-    }
+// FIXME: should be able to manually set the following values.
+export const mockPomodoroTimer = {
+  work: 25 * 60 * 1000,
+  shortBreak: 5 * 60 * 1000,
+  longBreak: 15 * 60 * 1000,
+  status: TIMER_RUNNING_STATUS.work,
+  stateTransition: TIMER_STATE_TRANSITION.initial,
+  remainingTime: 25 * 60 * 1000,
+  setTime: 25 * 60 * 1000,
+  longBreakInterval: 4,
+  section: {
+    current: 1,
+    limit: 10
   },
+  intervalId: undefined // HACK: I wanna use null property
+};
+
+const initialState: State = {
+  pomodoro: JSON.parse(JSON.stringify(mockPomodoroTimer)),
   timer: { remainingTime: 0, isRunning: false, isFinished: false }
 };
 
@@ -28,30 +40,8 @@ export const StateContext = createContext<ContextType<State>>({
 });
 
 export function StateProvider(props: { children: JSX.Element }): JSX.Element {
-  // TODO: 記録設定された値から、pomodoro の初期値を設定する
+  // FIXME: should be able to manually set the following values.
   const [state, setState] = createStore(initialState);
-
-  console.log("state provider");
-
-  // Refactor: count down
-  let id = null;
-  onMount(() => {
-    id = setInterval(() => {
-      if (!state.pomodoro.isRunning) return;
-      console.log("count down", state.pomodoro.remainingTime);
-      setState("pomodoro", "remainingTime", (prev) => prev - 1000);
-      // 0秒以下になったらタイマーを止める
-      if (state.pomodoro.remainingTime <= 0) {
-        clearInterval(id);
-        setState("pomodoro", {
-          remainingTime: 0,
-          isRunning: false,
-          isFinished: true
-        });
-      }
-    }, 1000);
-  });
-  onCleanup(() => clearInterval(id));
 
   return (
     <StateContext.Provider value={{ state, setState }}>{props.children}</StateContext.Provider>
