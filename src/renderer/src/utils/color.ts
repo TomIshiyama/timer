1671e2a44976c13e5d20b9c9c5e29766d5839af1
TomIshiyama <honngannji@gmail.com>
2023-08-17 21:53:50 +0900

    :sparkles: feat: add a several semantic colors.

--- src/renderer/src/utils/color.ts
+++ src/renderer/src/utils/color.ts
@@ -1,3 +1,14 @@
+// X-EX01 Gundam Calibarn
+// Primitive Colors
+export const BLACK = "#151e1c";
+export const DARK_GRAY = "#303f3d";
+export const DIM_GRAY = "#445956";
+export const WHITE = "#d4d8e1";
+export const WHITE_DARKER = "#b8bbc3";
+export const SILVER = "#80858e";
+export const RED = "#b54441";
+export const YELLOW = "#eaca45";
+export const GREEN = "#5af387";
 export const COLOR = {
   dark: {
     base: {
