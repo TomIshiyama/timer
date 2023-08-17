c05fbc9eccd168269e070220f0bd898b672f72b4
TomIshiyama <honngannji@gmail.com>
2023-08-17 23:08:33 +0900

    :sparkles: feat: add some flags for the next state.

--- src/renderer/src/components/page/pomodoro/Container.tsx
+++ src/renderer/src/components/page/pomodoro/Container.tsx
@@ -3,5 +3,29 @@ import { PomodoroProps } from "./type";
 import { PomodoroPresentational } from "./Presentational";
 
 export const PomodoroContainer: Component<PomodoroProps> = (props) => {
-  return <PomodoroPresentational {...props} />;
+  const { state, setState } = useContext(StateContext);
+
+  const { getRect } = useWindowRect();
+
+  // HACK: create a new file for usePomodoroTimer hooks
+  const isNextShortBreak = createMemo(
+    () =>
+      state.pomodoro.status === TIMER_RUNNING_STATUS.work &&
+      state.pomodoro.section.current < state.pomodoro.section.limit
+  );
+
+  const isNextLongBreak = createMemo(
+    () =>
+      state.pomodoro.section.current % mockPomodoroTimer.longBreakInterval === 0 &&
+      isNextShortBreak()
+  );
+
+  const isNextFinish = createMemo(
+    () => state.pomodoro.section.current === state.pomodoro.section.limit
+  );
+
+  return (
+    <>
+    </>
+  );
 };
