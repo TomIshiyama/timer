2994d696307ac2682ff0c30f9f52f190a9ba3c11
TomIshiyama <honngannji@gmail.com>
2023-08-17 22:24:00 +0900

    :fire: remove: StateProvider was moved to sibling.

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,13 +1,7 @@
 import { render } from "solid-js/web";
 import "./assets/index.css";
 import App from "./App";
-import { StateProvider } from "./components/utility/StateProvider";
 
-render(
-  () => (
-    <StateProvider>
-      <App />
-    </StateProvider>
-  ),
-  document.getElementById("root") as HTMLElement
-);
+import "./assets/styles/fonts.css";
+
+render(() => <App />, document.getElementById("root") as HTMLElement);
