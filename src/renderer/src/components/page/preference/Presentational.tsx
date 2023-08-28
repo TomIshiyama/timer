d37788e118c126b0f0bc1f36a33d0a37d15a6195
TomIshiyama <honngannji@gmail.com>
2023-08-29 00:57:45 +0900

    :sparkles: feat: make a preference page at the least cost

--- src/renderer/src/components/page/preference/Presentational.tsx
+++ src/renderer/src/components/page/preference/Presentational.tsx
@@ -1,29 +1,67 @@
 import { Input } from "@renderer/components/ui/form/Input";
 import { Component } from "solid-js";
+import { styled } from "solid-styled-components";
 import { PreferenceProps } from "./type";
 
-const DEFAULT_VALUES = {
-  work: 25,
-  shortBreak: 5,
-  longBreak: 15,
-  longBreakInterval: 5,
-  sectionLimit: 10
-};
+const Wrapper = styled("div")`
+  width: 90%;
+`;
 
-export const Presentational: Component<PreferenceProps> = () => {
+export const Presentational: Component<PreferenceProps> = (props) => {
   return (
-    <>
+    <Wrapper>
       <h1>hello preference</h1>
 
-      <Input name="work" type="number" defaultValue={DEFAULT_VALUES.work} />
-      <Input name="shortBreak" type="number" defaultValue={DEFAULT_VALUES.shortBreak} />
-      <Input name="longBreak" type="number" defaultValue={DEFAULT_VALUES.longBreak} />
       <Input
+        label="work time"
+        placeholder="work time e.g. 25"
+        onChange={props.onChangeWork}
+        name="work"
+        type="number"
+        defaultValue={props.defaultValues.work}
+        max={100}
+        min={0}
+      />
+      <Input
+        label="short break time"
+        placeholder="short break time"
+        onChange={props.onChangeShortBreak}
+        name="shortBreak"
+        type="number"
+        defaultValue={props.defaultValues.shortBreak}
+        max={60}
+        min={0}
+      />
+      <Input
+        label="long break time"
+        placeholder="long break time"
+        onChange={props.onChangeLongBreak}
+        name="longBreak"
+        type="number"
+        defaultValue={props.defaultValues.longBreak}
+        max={60}
+        min={0}
+      />
+      <Input
+        label="long break interval"
+        placeholder="long break interval"
+        onChange={props.onChangeLongBreakInterval}
         name="longBreakInterval"
         type="number"
-        defaultValue={DEFAULT_VALUES.longBreakInterval}
+        defaultValue={props.defaultValues.longBreakInterval}
+        max={60}
+        min={0}
       />
-      <Input name="sectionLimit" type="number" defaultValue={DEFAULT_VALUES.sectionLimit} />
-    </>
+      <Input
+        label="section limit"
+        placeholder="section limit"
+        onChange={props.onChangeSectionLimit}
+        name="sectionLimit"
+        type="number"
+        defaultValue={props.defaultValues.sectionLimit}
+        max={60}
+        min={0}
+      />
+    </Wrapper>
   );
 };
