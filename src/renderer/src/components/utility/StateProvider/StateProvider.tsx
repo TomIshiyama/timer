bf8ce0ac348fccfd6810b418c40794f1e4c17e81
TomIshiyama <honngannji@gmail.com>
2023-08-29 23:29:28 +0900

    :sparkles: feat: enable to initialize the state with indexedDB

--- src/renderer/src/components/utility/StateProvider/StateProvider.tsx
+++ src/renderer/src/components/utility/StateProvider/StateProvider.tsx
@@ -6,6 +6,9 @@ import {
   TIMER_RUNNING_STATUS,
   TIMER_STATE_TRANSITION
 } from "@renderer/components/page/pomodoro/type";
+import { omit } from "@renderer/utils/util";
+import { db } from "../../../utils/dexie";
+import { DEFAULT_VALUES } from "../../page/preference/const";
 
 type ContextType<T = object> = {
   state: Store<T>;
@@ -31,8 +34,45 @@ export const mockPomodoroTimer = {
 
 const initialState: State = {
   pomodoro: JSON.parse(JSON.stringify(mockPomodoroTimer)),
-  timer: { remainingTime: 0, isRunning: false, isFinished: false }
+  timer: { remainingTime: 0, isRunning: false, isFinished: false },
+  preference: { ...DEFAULT_VALUES }
 };
+
+// This mounting is for setting initial state with indexedDB
+export async function fetchState(): Promise<State> {
+  const initPreference = await db.preference.get({ id: "preference" });
+
+  if (initPreference == null) {
+    await db.preference.add({
+      ...DEFAULT_VALUES,
+      id: "preference"
+    });
+  }
+  const { sectionLimit, ...preference } =
+    initPreference == null ? DEFAULT_VALUES : omit(initPreference, "id");
+  console.log("pre", preference);
+  return {
+    pomodoro: {
+      ...mockPomodoroTimer,
+      work: preference.work,
+      shortBreak: preference.shortBreak,
+      longBreak: preference.longBreak,
+      longBreakInterval: preference.longBreakInterval,
+      remainingTime: preference.work,
+      setTime: preference.work,
+      section: {
+        current: 1,
+        limit: sectionLimit
+      }
+    },
+    timer: {
+      remainingTime: 0,
+      isFinished: false,
+      isRunning: false
+    },
+    preference: { ...preference, sectionLimit }
+  };
+}
 
 export const StateContext = createContext<ContextType<State>>({
   state: initialState,
@@ -42,6 +82,11 @@ export const StateContext = createContext<ContextType<State>>({
 export function StateProvider(props: { children: JSX.Element }): JSX.Element {
   // FIXME: should be able to manually set the following values.
   const [state, setState] = createStore(initialState);
+
+  onMount(async () => {
+    const tmp = await fetchState();
+    setState(tmp);
+  });
 
   return (
     <StateContext.Provider value={{ state, setState }}>{props.children}</StateContext.Provider>
