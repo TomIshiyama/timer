ccf0e19506559744a6cbaac9315e0662f99e5c79
TomIshiyama <honngannji@gmail.com>
2023-08-30 01:15:28 +0900

    :sparkles: feat: make Container wrapper and use it

--- src/renderer/src/components/page/pomodoro/Presentational.tsx
+++ src/renderer/src/components/page/pomodoro/Presentational.tsx
@@ -7,44 +7,48 @@ import { getTimeWithObject } from "../../../utils/time";
 import { CircleCountDown } from "../../ui/CircleCountDown/CircleCountDown";
 import { SkipButton } from "../../ui/Button/SkipButton";
 import { SquareButton } from "../../ui/Button/SquareButton";
+import { Container } from "../../ui/Container/Container";
 
 export const PomodoroPresentational: Component<PomodoroProps> = (props) => {
   return (
-    <Style>
-      {props.stateTransition === TIMER_STATE_TRANSITION.done ? (
-        <>
-          <h1>Well Done!</h1>
-          <button onClick={props.onClickInitialize}> GO BACK </button>
-        </>
-      ) : (
-        <>
-          {/* TODO: fix display position */}
-          <p>session: {`${props.section.current} / ${props.section.limit}`}</p>
-          <div>
-            <CircleCountDown
-              size={props.size ?? 400}
-              setTime={props.setTime}
-              remainingTime={props.remainingTime}
-            />
-          </div>
-          <div>
-            <PlayPauseButton
-              size="large"
-              mode={
-                props.stateTransition === TIMER_STATE_TRANSITION.pause ||
-                props.stateTransition === TIMER_STATE_TRANSITION.initial
-                  ? "play"
-                  : "pause"
-              }
-              onPlayClick={props.onClickPlay}
-              onPauseClick={props.onClickPause}
-            />
-            <SkipButton size={"large"} onClick={props.onClickSkip} />
-            <SquareButton size={"large"} onClick={props.onClickForceFinish} />
-          </div>
-        </>
-      )}
-    </Style>
+    <Container>
+      <Style>
+        <h2>Pomodoro timer</h2>
+        {props.stateTransition === TIMER_STATE_TRANSITION.done ? (
+          <>
+            <h1>Well Done!</h1>
+            <button onClick={props.onClickInitialize}> GO BACK </button>
+          </>
+        ) : (
+          <>
+            {/* TODO: fix display position */}
+            <p>session: {`${props.section.current} / ${props.section.limit}`}</p>
+            <div>
+              <CircleCountDown
+                size={props.size ?? 400}
+                setTime={props.setTime}
+                remainingTime={props.remainingTime}
+              />
+            </div>
+            <div>
+              <PlayPauseButton
+                size="large"
+                mode={
+                  props.stateTransition === TIMER_STATE_TRANSITION.pause ||
+                  props.stateTransition === TIMER_STATE_TRANSITION.initial
+                    ? "play"
+                    : "pause"
+                }
+                onPlayClick={props.onClickPlay}
+                onPauseClick={props.onClickPause}
+              />
+              <SkipButton size={"large"} onClick={props.onClickSkip} />
+              <SquareButton size={"large"} onClick={props.onClickForceFinish} />
+            </div>
+          </>
+        )}
+      </Style>
+    </Container>
   );
 };
 
--- src/renderer/src/components/page/preference/Presentational.tsx
+++ src/renderer/src/components/page/preference/Presentational.tsx
@@ -2,66 +2,70 @@ import { Input } from "@renderer/components/ui/form/Input";
 import { Component } from "solid-js";
 import { styled } from "solid-styled-components";
 import { PreferenceProps } from "./type";
+import { Container } from "../../ui/Container/Container";
 
 const Wrapper = styled("div")`
-  width: 90%;
+  width: 100%;
+  display: flex;
 `;
 
 export const Presentational: Component<PreferenceProps> = (props) => {
   return (
     <Wrapper>
-      <h1>hello preference</h1>
+      <Container>
+        <h2>Preference</h2>
 
-      <Input
-        label="work time"
-        placeholder="work time e.g. 25"
-        onChange={props.onChangeWork}
-        name="work"
-        type="number"
-        defaultValue={props.defaultValues.work}
-        max={100}
-        min={0}
-      />
-      <Input
-        label="short break time"
-        placeholder="short break time"
-        onChange={props.onChangeShortBreak}
-        name="shortBreak"
-        type="number"
-        defaultValue={props.defaultValues.shortBreak}
-        max={60}
-        min={0}
-      />
-      <Input
-        label="long break time"
-        placeholder="long break time"
-        onChange={props.onChangeLongBreak}
-        name="longBreak"
-        type="number"
-        defaultValue={props.defaultValues.longBreak}
-        max={60}
-        min={0}
-      />
-      <Input
-        label="long break interval"
-        placeholder="long break interval"
-        onChange={props.onChangeLongBreakInterval}
-        name="longBreakInterval"
-        type="number"
-        defaultValue={props.defaultValues.longBreakInterval}
-        max={60}
-        min={0}
-      />
-      <Input
-        label="section limit"
-        placeholder="section limit"
-        onChange={props.onChangeSectionLimit}
-        name="sectionLimit"
-        type="number"
-        defaultValue={props.defaultValues.sectionLimit}
-        max={60}
-        min={0}
-      />
+        <Input
+          label="work time"
+          placeholder="work time e.g. 25"
+          onChange={props.onChangeWork}
+          name="work"
+          type="number"
+          defaultValue={props.defaultValues.work}
+          max={100}
+          min={0}
+        />
+        <Input
+          label="short break time"
+          placeholder="short break time"
+          onChange={props.onChangeShortBreak}
+          name="shortBreak"
+          type="number"
+          defaultValue={props.defaultValues.shortBreak}
+          max={60}
+          min={0}
+        />
+        <Input
+          label="long break time"
+          placeholder="long break time"
+          onChange={props.onChangeLongBreak}
+          name="longBreak"
+          type="number"
+          defaultValue={props.defaultValues.longBreak}
+          max={60}
+          min={0}
+        />
+        <Input
+          label="long break interval"
+          placeholder="long break interval"
+          onChange={props.onChangeLongBreakInterval}
+          name="longBreakInterval"
+          type="number"
+          defaultValue={props.defaultValues.longBreakInterval}
+          max={60}
+          min={0}
+        />
+        <Input
+          label="section limit"
+          placeholder="section limit"
+          onChange={props.onChangeSectionLimit}
+          name="sectionLimit"
+          type="number"
+          defaultValue={props.defaultValues.sectionLimit}
+          max={60}
+          min={0}
+        />
+      </Container>
     </Wrapper>
   );
 };
