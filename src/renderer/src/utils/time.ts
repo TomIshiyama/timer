e372959a6be9a6d8e6172a91f2072bd154d23ce0
TomIshiyama <honngannji@gmail.com>
2023-09-03 00:01:23 +0900

    :bug: fix: change floor to round.
    - number of time is unexpected.

--- src/renderer/src/utils/time.ts
+++ src/renderer/src/utils/time.ts
@@ -8,17 +8,17 @@ type Time = {
 
 export function getTimeWithObject(remainingTime: number): Time {
   return {
-    hours: Math.floor(remainingTime / 3600000),
-    minutes: Math.floor((remainingTime % 3600000) / 60000),
-    seconds: Math.floor(((remainingTime % 3600000) % 60000) / 1000)
+    hours: Math.round(remainingTime / 3600000),
+    minutes: Math.round((remainingTime % 3600000) / 60000),
+    seconds: Math.round(((remainingTime % 3600000) % 60000) / 1000)
   };
 }
 
 export function getTime(remainingTime: number): Time {
   return {
-    hours: Math.floor(remainingTime / 3600000),
-    minutes: Math.floor((remainingTime / (1000 * 60)) % 60),
-    seconds: Math.floor((remainingTime / 1000) % 60)
+    hours: Math.round(remainingTime / 3600000),
+    minutes: Math.round((remainingTime / (1000 * 60)) % 60),
+    seconds: Math.round((remainingTime / 1000) % 60)
   };
 }
 
@@ -28,8 +28,8 @@ export function timeToUnix(time: Time): number {
 
 export function getTimeWithoutHours(remainingTime: number): Omit<Time, "hours"> {
   return {
-    minutes: Math.floor(remainingTime / (1000 * 60)),
-    seconds: Math.floor((remainingTime / 1000) % 60)
+    minutes: Math.round(remainingTime / (1000 * 60)),
+    seconds: Math.round((remainingTime / 1000) % 60)
   };
 }
 
