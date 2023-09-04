90d6b8145393f7f6eaaa176b5388d0a386c22935
TomIshiyama <honngannji@gmail.com>
2023-09-05 00:19:24 +0900

    feat: update styles of SkipButton.

--- src/renderer/src/components/ui/Button/SkipButton.tsx
+++ src/renderer/src/components/ui/Button/SkipButton.tsx
@@ -1,29 +1,35 @@
 import { Component } from "solid-js";
 import { styled } from "solid-styled-components";
-import { Play } from "./PlayPauseButton";
+import { ButtonWrapper, Play } from "./PlayPauseButton";
 import { JSX } from "solid-js";
 
 type Props = {
-  size: "small" | "medium" | "large";
+  size: "atom" | "small" | "medium" | "large";
+  color?: string;
   onClick?: JSX.EventHandlerUnion<HTMLButtonElement, MouseEvent> | undefined;
 };
 
 const sizeMap = {
+  atom: 24,
   small: 32,
   medium: 48,
   large: 64
 } as const;
 
 const VerticalLine = styled("div")``;
+const Button = styled("button")`
+  background: transparent;
+  border: none;
+`;
 
 const SkipButtonComponent: Component<Props & { class?: string }> = (props) => {
   return (
-    <button onClick={props.onClick}>
+    <ButtonWrapper size={sizeMap[props.size]} onClick={props.onClick}>
       <div class={props.class}>
-        <Play initialSize={sizeMap[props.size] ?? sizeMap.medium} />
+        <Play initialSize={sizeMap[props.size] ?? sizeMap.medium} color={props.color} />
         <VerticalLine />
       </div>
-    </button>
+    </ButtonWrapper>
   );
 };
 
@@ -33,12 +39,16 @@ export const SkipButton = styled(SkipButtonComponent)<Props>((props) => ({
   width: `${sizeMap[props.size]}px`,
   height: `${(sizeMap[props.size] * 2) / 3}px`,
   position: "relative",
+  "& > div": {
+    transform: "none"
+  },
   "& > div + div": {
     width: `${sizeMap[props.size] / 12}px`,
     height: `${(sizeMap[props.size] * 2) / 3}px`,
     position: "absolute",
-    left: `${(sizeMap[props.size] * 2) / 3}px`,
+    // left: `${(sizeMap[props.size] * 2) / 3}px`,
     top: 0,
-    backgroundColor: "dimgray"
+    backgroundColor: props.color ?? "dimgray",
+    transform: `translateX(${sizeMap[props.size] / 3}px)`
   }
 }));
