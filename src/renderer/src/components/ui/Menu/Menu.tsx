7bbed2e959f38a583cf62bc3b6786b6cd6777356
TomIshiyama <honngannji@gmail.com>
2023-09-05 00:23:56 +0900

    :sparkles: feat: able to toggle this menu.

--- src/renderer/src/components/ui/Menu/Menu.tsx
+++ src/renderer/src/components/ui/Menu/Menu.tsx
@@ -1,6 +1,12 @@
-import { Component } from "solid-js";
+import { Component, createSignal, onMount } from "solid-js";
 import { Tab } from "../Tab/Tab";
 import { useNavigate } from "@solidjs/router";
+import { readFileSync } from "fs";
+import { IconTomatoSVG } from "../../../assets/IconTomatoSVG";
+import { IconGearSVG } from "../../../assets/IconGearSVG";
+import { BackButton } from "../Button/BackButton";
+import { COLOR } from "../../../utils/color";
+import { styled } from "solid-styled-components";
 
 type Props = { wip?: unknown };
 
@@ -16,10 +22,82 @@ const tabList = [
   }
 ];
 
+const tabListWithIcon = [
+  // { value: "/", label: "Home" },
+  {
+    value: "/pomodoro", // should set URL path
+    label: (
+      <>
+        <IconTomatoSVG /> Pomodoro
+      </>
+    ) // should use icon
+  },
+  {
+    value: "/preference",
+    label: (
+      <>
+        <IconGearSVG /> Preference
+      </>
+    )
+  }
+];
+
+const tabListOnlyIcon = [
+  // { value: "/", label: "Home" },
+  {
+    value: "/pomodoro", // should set URL path
+    label: (
+      <>
+        <IconTomatoSVG />
+      </>
+    ) // should use icon
+  },
+  {
+    value: "/preference",
+    label: (
+      <>
+        <IconGearSVG />
+      </>
+    )
+  }
+];
+
 export const Menu: Component<Props> = (_) => {
   const navigate = useNavigate();
-
+  const [getOpen, setOpen] = createSignal(true);
+  onMount(() => {
+    navigate("/pomodoro");
+  });
   return (
-    <Tab tabList={tabList} orientation={"vertical"} onChange={(path): void => navigate(path)} />
+    <div style={{ position: "relative", "border-right": `1px solid ${COLOR.dark.footer.border}` }}>
+      <Tab
+        disableBorder
+        tabList={getOpen() ? tabListWithIcon : tabListOnlyIcon}
+        orientation={"vertical"}
+        onChange={(path): void => navigate(path)}
+        defaultValue="/pomodoro"
+      />
+      <Button
+        onClick={(): void => {
+          setOpen((prev) => !prev);
+        }}
+      >
+        {getOpen() ? "<" : ">"}
+      </Button>
+    </div>
   );
 };
+
+const Button = styled("button")((props) => ({
+  background: "transparent",
+  position: "absolute",
+  bottom: 0,
+  padding: "8px",
+  color: "white",
+  border: "none",
+  cursor: "pointer",
+  right: 0,
+  "&:hover": {
+    backgroundColor: COLOR.dark.tab.hover
+  }
+}));
