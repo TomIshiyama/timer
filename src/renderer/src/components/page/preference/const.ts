d49384a2b39583e3ff9d7d3a014d5cca59b9f316
TomIshiyama <honngannji@gmail.com>
2023-09-06 21:43:00 +0900

    :sparkles: feat: update a constant of preference

--- src/renderer/src/components/page/preference/const.ts
+++ src/renderer/src/components/page/preference/const.ts
@@ -1,5 +1,5 @@
-export const DEFAULT_VALUES = {
 import { PreferenceState } from "./type";
+
 export const DEFAULT_VALUES: PreferenceState = {
   work: 25 * 60000,
   shortBreak: 5 * 60000,
@@ -7,4 +7,12 @@ export const DEFAULT_VALUES: PreferenceState = {
   longBreakInterval: 5,
   sectionLimit: 10,
   opacity: 1,
+  sounds: {
+    volume: 1.0,
+    work: "clock1",
+    shortBreak: "river",
+    longBreak: "pond",
+    turnOver: "pigeon",
+    almostDone: "timerHarry"
+  }
 };
